c10ffd64c0b0cbe663066efcda7f9564ed302124
Mitch Garnaat <mitch@scopely.com>
2015-04-23 15:05:42 -0700

    Added a requirements.txt file to make it easier to install dependencies.  Added .pyc file to .gitignore.  Cleaned up resources.py a bit, used print function rather than print statement. Removed some semi-colons (wat?), fixed some formatting.

--- resources.py
+++ resources.py
@@ -42,16 +42,15 @@ from docopt import docopt
 import yaml
 from boto import cloudformation
 from troposphere import Template, Parameter, Output
-from troposphere import Base64, FindInMap, GetAtt, GetAZs, Join, Ref, Select, Tags
-import troposphere.ec2 as ec2
-import troposphere.autoscaling as asg
+from troposphere import GetAtt, Ref
 import troposphere.iam as iam
 import troposphere.sns as sns
 from awacs.aws import Action, Allow, Policy, Statement, Principal, Condition, StringEquals
 
 
 def default_config():
-    """Returns a dict with the default configuration
+    """
+    Returns a dict with the default configuration
     """
     return {
         'stack_name': 'Lambda-Chat',
@@ -63,14 +62,15 @@ def default_config():
 
 
 def load_config():
-    """Returns the default config merged with the what is in the config.yml file
+    """
+    Returns the default config merged with the what is in the config.yml file
     """
     try:
         # Attempt to load configuration file
-        stream = file('config.yml', 'r');
+        stream = file('config.yml', 'r')
         config = yaml.load(stream)
         config['loaded'] = True
-    except IOError, e:
+    except IOError:
         config = {}
         config['loaded'] = False
 
@@ -79,16 +79,18 @@ def load_config():
 
 
 def assert_config_loaded():
-    """Stops execution and displays an error message if the settings have not
+    """
+    Stops execution and displays an error message if the settings have not
     been loaded from config.yml
     """
     if not config['loaded']:
-        print 'ERROR: Could not load file: config.yml'
+        print('ERROR: Could not load file: config.yml')
         sys.exit(1)
 
 
 def cf_params():
-    """Returns the parameters needed to create or update the CloudFormation
+    """
+    Returns the parameters needed to create or update the CloudFormation
     stack
     """
     assert_config_loaded()
@@ -98,22 +100,22 @@ def cf_params():
 
 
 def generate_cf_template():
-    """Returns an entire CloudFormation stack by using troposphere to construct
+    """
+    Returns an entire CloudFormation stack by using troposphere to construct
     each piece
     """
     # Header of CloudFormation template
     t = Template()
     t.add_version("2010-09-09")
     t.add_description("Lambda Chat AWS Resources")
-
-
     # Paramters
+    description = "should match [0-9]+-[a-z0-9]+.apps.googleusercontent.com"
     google_oauth_client_id = t.add_parameter(Parameter(
         "GoogleOAuthClientID",
         AllowedPattern="[0-9]+-[a-z0-9]+.apps.googleusercontent.com",
         Type="String",
         Description="The Client ID of your Google project",
-        ConstraintDescription="should match [0-9]+-[a-z0-9]+.apps.googleusercontent.com",
+        ConstraintDescription=description
     ))
 
     # s3_bucket_name = t.add_parameter(Parameter(
@@ -121,9 +123,9 @@ def generate_cf_template():
     #     AllowedPattern="[a-zA-Z0-9\-]*",
     #     Type="String",
     #     Description="Name of S3 bucket to run the website from",
-    #     ConstraintDescription="can contain only alphanumeric characters and dashes.",
+    #     ConstraintDescription="can contain only alphanumeric characters
+    #     and dashes.",
     # ))
-
 
     # The SNS topic the website will publish chat messages to
     website_sns_topic = t.add_resource(sns.Topic(
@@ -131,7 +133,7 @@ def generate_cf_template():
         TopicName='lambda-chat',
         DisplayName='Lambda Chat'
     ))
-    output_website_sns_topic = t.add_output(Output(
+    t.add_output(Output(
         "WebsiteSnsTopic",
         Description="website_sns_topic_arn",
         Value=Ref(website_sns_topic),
@@ -157,7 +159,7 @@ def generate_cf_template():
             ],
         ),
     ))
-    website_policy = t.add_resource(iam.PolicyType(
+    t.add_resource(iam.PolicyType(
         "WebsitePolicy",
         PolicyName="lambda-chat-website-policy",
         Roles=[Ref(website_role)],
@@ -173,8 +175,8 @@ def generate_cf_template():
                 ),
             ],
         )
-    ));
-    output_website_role = t.add_output(Output(
+    ))
+    t.add_output(Output(
         "WebsiteRole",
         Description="website_iam_role_arn",
         Value=GetAtt(website_role, "Arn"),
@@ -184,48 +186,51 @@ def generate_cf_template():
 
 
 def launch(args, config, cf_conn, template):
-    """Create new CloudFormation Stack from the template
     """
-    print "Creating CloudFormation Stack %s..." % config['stack_name']
+    Create new CloudFormation Stack from the template
+    """
+    print("Creating CloudFormation Stack %s..." % config['stack_name'])
     stack_id = cf_conn.create_stack(
-      config['stack_name'],
-      template_body=template.to_json(),
-      parameters=cf_params(),
-      tags=config['tags'],
-      capabilities=['CAPABILITY_IAM']
+        config['stack_name'],
+        template_body=template.to_json(),
+        parameters=cf_params(),
+        tags=config['tags'],
+        capabilities=['CAPABILITY_IAM']
     )
-    print 'Created ' + stack_id
+    print('Created ' + stack_id)
 
 
 def update(args, config, cf_conn, template):
-    """Update an existing CloudFormation Stack
     """
-    print "Updating CloudFormation Stack %s..." % config['stack_name']
+    Update an existing CloudFormation Stack
+    """
+    print("Updating CloudFormation Stack %s..." % config['stack_name'])
     stack_id = cf_conn.update_stack(
-      config['stack_name'],
-      template_body=template.to_json(),
-      parameters=cf_params(),
-      tags=config['tags'],
-      capabilities=['CAPABILITY_IAM']
+        config['stack_name'],
+        template_body=template.to_json(),
+        parameters=cf_params(),
+        tags=config['tags'],
+        capabilities=['CAPABILITY_IAM']
     )
-    print 'Updated ' + stack_id
+    print('Updated ' + stack_id)
 
 
 def delete(args, config, cf_conn):
-    """Deletes an existing CloudFormation Stack
+    """
+    Deletes an existing CloudFormation Stack
     """
     # Delete an existing CloudFormation Stack with same name
-    print "Deleting CloudFormation Stack %s..." % config['stack_name']
+    print("Deleting CloudFormation Stack %s..." % config['stack_name'])
     resp = cf_conn.delete_stack(
-      config['stack_name'],
+        config['stack_name'],
     )
-    print resp
+    print(resp)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='Lambda Chat AWS Resources 0.2')
     print(args)
-    config = load_config();
+    config = load_config()
     print(config)
 
     print_cf_template = args['cf'] or args['launch']
@@ -233,13 +238,12 @@ if __name__ == '__main__':
     try:
         if print_cf_template:
             template = generate_cf_template()
-            print template.to_json();
+            print(template.to_json())
             if (args['cf']):
                 sys.exit(1)
 
         # Get a connection to AWS CloudFormation in the given region
         conn = cloudformation.connect_to_region(args['--region'])
-
 
         if (args['launch']):
             launch(args, config, conn, template)
@@ -252,13 +256,13 @@ if __name__ == '__main__':
 
         elif (args['output']):
             # Describe an existing CloudFormation Stack
-            print "Deleting CloudFormation Stack..."
+            print("Deleting CloudFormation Stack...")
             resp = conn.describe_stacks(
-              stack_name,
+                config['stack_name']
             )
-            print resp
+            print(resp)
 
     except Exception, e:
-        print 'ERROR'
-        print e
-        print e.message
+        print('ERROR')
+        print(e)
+        print(e.message)
