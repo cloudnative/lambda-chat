2162d123eca138efa9a673286eef797ca5d1e5e0
Mitch Garnaat <mitch@scopely.com>
2015-04-26 10:09:00 -0700

    Adding initial versions of kappa config files and related code to create the SNS->DynamoDB lambda function and the DynamoDB->S3 lambda function.  This requires the latest version of kappa which exists only on a branch of the kappa repo.

---
+++ lambda/sns/lambdachat.js
@@ -0,0 +1,21 @@
+console.log('Loading event');
+var aws = require('aws-sdk');
+var ddb = new aws.DynamoDB(
+    {endpoint: 'https://preview-dynamodb.us-east-1.amazonaws.com/',
+     params: {TableName: 'lambdachat'}});
+ 
+exports.handler = function(event, context) {
+    var message_id = event.Records[0].Sns.MessageId;
+    var timestamp = event.Records[0].Sns.Timestamp;
+    var payload = JSON.parse(event.Records[0].Sns.Message)
+    var name = payload.name
+    var message = payload.message
+    var LambdaReceiveTime = new Date().toString();
+    var itemParams = {Item: {message_id: {S: message_id},
+                             timestamp: {S: timestamp},
+                             name: {S: name},
+                             message: {S: message}}};
+    ddb.putItem(itemParams, function() {
+        context.done(null,'');
+    });
+};
