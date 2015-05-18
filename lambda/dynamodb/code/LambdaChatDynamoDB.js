03847e085ff54eb9eaefa02248b376e93e437347
Mitch Garnaat <mitch@scopely.com>
2015-05-18 07:23:12 -0700

    Fixed a bug in the DynamoDB controller.  It wasn't scanning the table in reverse order and therefore was not returning latest results.

--- lambda/dynamodb/code/LambdaChatDynamoDB.js
+++ lambda/dynamodb/code/LambdaChatDynamoDB.js
@@ -2,23 +2,23 @@ console.log('Loading function');
 var async = require('async');
 var aws = require('aws-sdk');
 var ddb = new aws.DynamoDB(
-    {endpoint: 'https://preview-dynamodb.us-east-1.amazonaws.com/',
-     params: {TableName: 'lambdachat'}});
+    {endpoint: "https://preview-dynamodb.us-east-1.amazonaws.com/",
+     params: {TableName: "lambdachat"}});
 var s3 = new aws.S3();
 // change bucket name to match your bucket name
-var bucket = 'lambda-chat';
-var keyname = 'data.json';
+var bucket = "lambda-chat";
+var keyname = "data.json";
 
 
 exports.handler = function(event, context) {
     console.log('Handle DynamoDB Streams event');
     console.log(event);
     var new_object = event.Records[0].Dynamodb;
-    var channel = 'default';
+    var channel = "default";
     if ('channel' in new_object.NewImage) {
         channel = new_object.NewImage.channel.S;
     }
-    console.log('channel = ' + channel);
+    console.log("channel = " + channel);
 
 
     async.waterfall([
@@ -28,36 +28,38 @@ exports.handler = function(event, context) {
                 "KeyConditions": {
                     "channel": {
                         "AttributeValueList": [{
-                            "S": 'default'
+                            "S": "default"
                         }],
                         "ComparisonOperator": "EQ"
                     }
 
                 },
-                "Limit": "20",
+                "Limit": 20,
+                "ScanIndexForward": false
             }            
-            console.log('Scanning the table');
+            console.log("Scanning the table");
             ddb.query(params, next);
         },
         function buildjson(response, next) {
-            console.log('Building JSON file');
-            console.log(response);
+            console.log("Building JSON file");
+            console.log(JSON.stringify(response));
             var messageData = {
                 messages: []
             };
-            
-            for (var ii in response.Items) {
-                ii = response.Items[ii];
+
+            for (var i = response.Items.length - 1; i >= 0; i--) {
+                ii = response.Items[i];
                 var message = {};
                 message['id'] = ii.message_id['S'];
                 message['name'] = ii.name['S'];
                 message['message'] = ii.message['S'];
                 message['channel'] = ii.message['S'];
                 messageData.messages.push(message);
-            }
+            }            
             next(null, JSON.stringify(messageData));
         },
         function savetos3(jsonstring, next) {
+            console.log(jsonstring);
             s3.putObject({
                 Bucket: bucket,
                 Key: keyname,
